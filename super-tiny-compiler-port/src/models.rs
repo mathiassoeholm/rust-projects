8b71dcf8cdaa435aa914ce0450497ec1df28e996
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-04-26 21:02:06 +0200

    Add Node type

--- super-tiny-compiler-port/src/models.rs
+++ super-tiny-compiler-port/src/models.rs
@@ -11,3 +11,14 @@ pub struct Token {
   pub kind: TokenKind,
   pub value: String,
 }
+
+#[derive(Debug, PartialEq)]
+pub enum Node<'a> {
+  CallExpression {
+    name: &'a String,
+    params: Vec<Node<'a>>,
+  },
+  NumberLiteral {
+    value: &'a String,
+  },
+}
--- super-tiny-compiler-port/tests/parser_test.rs
+++ super-tiny-compiler-port/tests/parser_test.rs
@@ -1,17 +1,22 @@
-use super_tiny_compiler::models::{Token, TokenKind};
+use super_tiny_compiler::models::{Ast, Node, Token, TokenKind};
 use super_tiny_compiler::parser::*;
 
 #[test]
 fn it_returns_ast() {
   assert_eq!(
     vec!(Token {
-      kind: TokenKind::Paren,
-      value: "(".to_owned()
+      kind: TokenKind::Name,
+      value: "(add 1 2)".to_owned()
     }),
     Ast {
-      type: NodeType::Program,
-      body: vec!(
-      )
+      kind: Node::Program,
+      body: vec!(Node::CallExpression {
+        name: "add",
+        params: vec!(
+          Node::NumberLiteral { value: "1" },
+          Node::NumberLiteral { value: "2" }
+        )
+      })
     }
   );
 }
